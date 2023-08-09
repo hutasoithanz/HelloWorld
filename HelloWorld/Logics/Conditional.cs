9a84defd32bce9a4ae67b3c7fdbf18fdd7ada03d
Lenovo <Lenovo@LAPTOP-E51PO7NI>
2023-08-09 18:23:24 +0700

    Add project files.

---
+++ HelloWorld/Logics/Conditional.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Logics
+{
+    /// <summary>
+    /// Class adalah sebuah representasi objek di dunia nyata yang melakukan berbagai fungsi.
+    /// Fungsi tersebut didefinisikan dalam satu kumpulan kode (statement).
+    /// Alangkah baiknya jika tiap class melakukan fungsi objek dunia nyata yang dilakukannya.
+    /// Contoh: class Matematika akan melakukan fungsi matematika, bukan perbandingan huruf/kata.
+    /// Contoh buruk: class Gelas melakukan fungsi mengaduk semen.
+    /// 
+    /// Class conditional berisi berbagai jenis kondisi yang dapat digunakan dalam bahasa pemograman.
+    /// Di dalam ini contohnya sebagai berikut:
+    ///     - If Else; penggunaannya untuk nilai variabel yang tidak tetap atau bersifat ranged.
+    ///     - Switch Case; penggunaannya untuk nilai variabel yang sudah diketahui jenis dan nilainya.
+    /// </summary>
+    public class Conditional
+    {
+        /// <summary>
+        /// Ini adalah fungsi. Fungsi dalam class melakukan sesuatu tindakan dan bisa memberikan hasil.
+        /// Tidak semua fungsi memberikan hasil. Ada fungsi yang hanya melakukan pengerjaan di belakang.
+        /// 
+        /// Fungsi dapat juga dipanggil berdasarkan inisialisasinya. Fungsi 'static' akan tetap ada selama
+        /// program berjalan. Fungsi tanpa keyword 'static' akan ada jika object masih ada di memory.
+        /// 
+        /// Fungsi dapat memiliki variabel yang harus diberikan ketika pemanggilan fungsinya.
+        /// Variabel ini disebut dengan parameter. Parameter bisa berupa input ataupun input/output.
+        /// 
+        /// Pada contoh fungsi ini, akan didemonstrasikan cara pemanggilan conditional if
+        /// untuk perbandingan kedua parameter, mana yang lebih besar.
+        /// </summary>
+        /// <param name="angkaSatu">Parameter angkaSatu membutuhkan nilai angka integer untuk diisi.</param>
+        /// <param name="angkaDua">Parameter angkaDua membutuhkan nilai angka integer untuk diisi.</param>
+        /// <returns>Hasil perbandingan akan diberikan oleh fungsi ini kepada 
+        /// program yang memanggilnya.</returns>
+        public static string ContohConditionalIf(int angkaSatu, int angkaDua)
+        {
+            if (angkaSatu > angkaDua)
+            {
+                return "nilai " + angkaSatu + " lebih besar dari " + angkaDua + ".";
+            }
+            else if (angkaSatu < angkaDua)
+            {
+                return "nilai " + angkaSatu + " lebih kecil dari " + angkaDua + ".";
+
+            }
+            else
+            {
+                return "nilai " + angkaSatu + " sama dengan " + angkaDua + ".";
+            }
+        }
+
+        public static string ContohConditionalSwitch(int angkaInput)
+        {
+            switch (angkaInput % 2)
+            {
+                case 1:
+                    return "angka " + angkaInput + " adalah angka ganjil.";
+                case 0:
+                    return "angka " + angkaInput + " adalah angka genap.";
+                default:
+                    return "angka masukan tidak valid.";
+            }
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,41 @@
+﻿using HelloWorld.Logics;
+
+/// Variabel adalah sebuah penampung nilai dalam programming. 
+/// Variabel akan membooking tempat di memori sebagai penampung untuk nilai yang akan diberikan padanya.
+/// Nilai ini dapat diisi sesuai tipe yang dipilih.
+/// Bisa berupa angka (int, float, double), huruf (char, string) atau bahkan berupa object (Orang).
+/// Dalam bahasa pemograman modern, bisa juga tipe ini tidak didefinisikan.
+/// Misalnya dalam .NET tipe tanpa definisi adalah (var, obj).
+/// 
+/// Selain memiliki tipe, variabel juga memiliki sifat, 'static', 'final'/'readonly', 'const'.
+/// 'static' variabel akan selalu ada di memori selama program berjalan.
+/// 'final' atau 'readonly' akan memiliki value yang tidak dapat dirubah selama program berjalan.
+/// 'const' memiliki sifat yang sama dengan 'final' atau 'readonly'.
+/// Perbedaan 'const' dan 'readonly' ada di saat nilai diberikan. 'const' akan diberikan saat kompilasi
+/// sementara 'readonly' akan memiliki nilai saat program dijalankan.
+/// 
+int angkaSatu = 0; // selalu berikan nilai untuk variabel ketika inisialisasi.
+int angkaDua = 0;
+/// Hello world adalah hal pertama yang dikenalkan dalam pemograman.
+/// Ini menunjukkan pemogram menyapa dunia dengan program pertamanya.
+Console.WriteLine("Hello, World!");
+Console.WriteLine();
+/// Conditional atau perbandingan bisa dilihat di sini.
+Console.WriteLine("=================================================");
+Console.WriteLine("==CONDITIONALS===================================");
+Console.WriteLine("==CONDITIONAL IF=================================");
+Console.Write("Masukkan angka pertama: ");
+Int32.TryParse(Console.ReadLine(), out angkaSatu);
+Console.Write("Masukkan angka ke dua: ");
+Int32.TryParse(Console.ReadLine(), out angkaDua);
+/// Berikut ini adalah pemanggilan sebuah nilai hasil pengerjaan fungsi yang dilakukan objek statis.
+/// Arahkan kursor pada kata ContohConditionalIf lalu tekan F12 pada keyboard untuk mengetahui isi detil
+/// dari fungsi tersebut.
+Console.WriteLine(Conditional.ContohConditionalIf(angkaSatu, angkaDua));
+Console.WriteLine("==CONDITIONAL SWITCH=============================");
+Console.Write("Masukkan sebuah angka: ");
+Int32.TryParse(Console.ReadLine(), out angkaDua);
+Console.WriteLine(Conditional.ContohConditionalSwitch(angkaDua));
+/// Iteration atau pengulangan bisa dilihat di sini.
+Console.WriteLine("=================================================");
+Console.WriteLine("==ITERATION======================================");
